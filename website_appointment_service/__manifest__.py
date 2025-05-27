bffacb2666df68664523556894c54373043102af
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-27 13:23:40 +0300

    Add files via upload

---
+++ website_appointment_service/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Website Appointment with Service and Attachment",
+    "version": "1.0",
+    "category": "Website",
+    "summary": "Allow customers to book appointments, upload files, and create sales orders",
+    "depends": ["website", "calendar", "sale_management", "appointment"],
+    "data": [
+        "views/website_appointment_form.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
---
+++ website_appointment_service/controllers/website_appointment_controller.py
@@ -0,0 +1,44 @@
+
+from odoo import http
+from odoo.http import request
+import base64
+
+class WebsiteAppointmentExtended(http.Controller):
+
+    @http.route('/website/appointment', type='http', auth='public', website=True)
+    def render_appointment_form(self, **kwargs):
+        services = request.env['product.product'].sudo().search([('sale_ok', '=', True), ('type', '=', 'service')])
+        return request.render('website_appointment_service.website_appointment_custom_form', {
+            'services': services
+        })
+
+    @http.route('/website/appointment/submit', type='http', auth='public', website=True, csrf=True)
+    def appointment_submit(self, **post):
+        uploaded_file = request.httprequest.files.get('attachment_file')
+        product_id = int(post.get('service_product_id'))
+        product = request.env['product.product'].sudo().browse(product_id)
+        partner = request.env.user.partner_id
+
+        appointment = request.env['calendar.event'].sudo().create({
+            'name': post.get('name'),
+            'partner_ids': [(4, partner.id)],
+        })
+
+        if uploaded_file:
+            request.env['ir.attachment'].sudo().create({
+                'name': uploaded_file.filename,
+                'datas': base64.b64encode(uploaded_file.read()),
+                'res_model': 'calendar.event',
+                'res_id': appointment.id,
+                'type': 'binary',
+            })
+
+        order = request.env['sale.order'].sudo().create({
+            'partner_id': partner.id,
+            'order_line': [(0, 0, {
+                'product_id': product.id,
+                'product_uom_qty': 1,
+            })]
+        })
+
+        return request.redirect('/website/appointment/thank-you')
