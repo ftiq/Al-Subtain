f879b5b7d4c934c90d03809e080adcf3cbd07322
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-27 14:41:50 +0300

    Update __manifest__.py

--- custom_appointment_form_complete/__manifest__.py
+++ custom_appointment_form_complete/__manifest__.py
@@ -4,9 +4,10 @@
     "category": "Website",
     "summary": "Enhances appointment form to include service selection and file upload, saved into calendar.event",
     "depends": ["website", "appointment", "calendar", "sale_management"],
-    "data": [
-        "views/website_appointment_form.xml",
-        "views/calendar_event_form.xml"
+   "data": [
+    "views/appointment_extra_fields.xml",
+    "views/website_appointment_form.xml",
+    "views/calendar_event_form.xml"
     ],
     "installable": True,
     "application": False
