0fc931dd820c1811ff2034b6075297ceb1bdd35c
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-27 13:44:52 +0300

    Update __manifest__.py

--- website_appointment_service/__manifest__.py
+++ website_appointment_service/__manifest__.py
@@ -1,12 +1,13 @@
 {
-    "name": "Website Appointment with Service and Attachment",
-    "version": "1.0",
+    "name": "Website Appointment: Services and Attachments",
+    "version": "1.1",
     "category": "Website",
-    "summary": "Allow customers to book appointments, upload files, and create sales orders",
+    "summary": "Add service selection and file upload to website appointment and show attachments",
     "depends": ["website", "calendar", "sale_management", "appointment"],
     "data": [
         "views/website_appointment_form.xml",
+        "views/calendar_event_form.xml"
     ],
     "installable": True,
-    "application": True,
+    "application": True
 }
