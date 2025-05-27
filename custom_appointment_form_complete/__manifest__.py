ad64757e448a9ce39c70f2fdd4000d6febf3d60e
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-27 14:36:27 +0300

    Add files via upload

---
+++ custom_appointment_form_complete/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Custom Appointment Form: Service + File Upload",
+    "version": "1.0",
+    "category": "Website",
+    "summary": "Enhances appointment form to include service selection and file upload, saved into calendar.event",
+    "depends": ["website", "appointment", "calendar", "sale_management"],
+    "data": [
+        "views/website_appointment_form.xml",
+        "views/calendar_event_form.xml"
+    ],
+    "installable": True,
+    "application": False
+}
