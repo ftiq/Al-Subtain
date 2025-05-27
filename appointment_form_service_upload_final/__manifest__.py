509aea359218e14aed1aec9659d8e85541b29e45
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-27 14:21:15 +0300

    Add files via upload

---
+++ appointment_form_service_upload_final/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Appointment Form: Service + Attachment (Final)",
+    "version": "1.0",
+    "category": "Website",
+    "summary": "Add service dropdown and file upload to built-in appointment form (calendar.event safe)",
+    "depends": ["website", "appointment", "calendar", "sale_management"],
+    "data": [
+        "views/website_form_override.xml",
+        "views/calendar_event_form.xml"
+    ],
+    "installable": True,
+    "application": False
+}
