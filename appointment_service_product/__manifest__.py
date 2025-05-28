a43e06bf1d2791a670faf7052b6bd2825380a26a
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-29 01:49:04 +0300

    Update __manifest__.py

--- appointment_service_product/__manifest__.py
+++ appointment_service_product/__manifest__.py
@@ -1,15 +1,10 @@
 {
-    'name': 'Calendar Event Product Selection',
-    'version': '18.0.1.0.0',
-    'summary': 'Add product/service selection to calendar events',
-    'description': 'Allows selecting products/services when booking calendar appointments',
-    'author': 'Your Name',
-    'depends': ['calendar', 'website_calendar', 'product'],
+    'name': 'Calendar Product Selection',
+    'version': '1.0',
+    'depends': ['website_calendar', 'product'],
     'data': [
         'views/calendar_event_views.xml',
-        'views/website_calendar_templates.xml',
+        'views/website_templates.xml',
     ],
     'installable': True,
-    'application': False,
-    'license': 'LGPL-3',
 }
