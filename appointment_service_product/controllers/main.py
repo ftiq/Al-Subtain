e21e599ec07b73e53e7a8deeef8702c1197428b3
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-29 01:49:42 +0300

    Update main.py

--- appointment_service_product/controllers/main.py
+++ appointment_service_product/controllers/main.py
@@ -1,14 +1,12 @@
 from odoo import http
 from odoo.addons.website_calendar.controllers.main import WebsiteCalendarController
 
-class CustomWebsiteCalendarController(WebsiteCalendarController):
+class ProductCalendarController(WebsiteCalendarController):
 
-    @http.route()
-    def calendar_appointment(self, **kwargs):
-        response = super().calendar_appointment(**kwargs)
-        if response.qcontext:
-            response.qcontext['products'] = http.request.env['product.product'].search([
-                ('type', '=', 'service'),
-                ('sale_ok', '=', True)
-            ])
-        return response
+    def _prepare_calendar_appointment_values(self, **kwargs):
+        values = super()._prepare_calendar_appointment_values(**kwargs)
+        values['products'] = http.request.env['product.product'].search([
+            ('type', '=', 'service'),
+            ('sale_ok', '=', True)
+        ])
+        return values
