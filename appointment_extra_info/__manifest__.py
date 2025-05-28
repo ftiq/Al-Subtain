513ee38c2e6c16f07a07dedf7a31d577e1e11602
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-28 22:05:50 +0300

    Update __manifest__.py

--- appointment_extra_info/__manifest__.py
+++ appointment_extra_info/__manifest__.py
@@ -1,11 +1,12 @@
 {
     "name": "Appointment Extra Info",
-    "version": "1.0",
+    "version": "1.1",
     "depends": ["website", "appointment", "sale_management"],
     "category": "Website",
-    "summary": "Add a separate page for service and attachment after appointment confirmation",
+    "summary": "Add extra page after appointment for service and file upload",
     "data": [
-        "views/appointment_extra_template.xml"
+        "views/appointment_extra_template.xml",
+        "views/appointment_thank_you_inherit.xml"
     ],
     "installable": True,
     "application": False
