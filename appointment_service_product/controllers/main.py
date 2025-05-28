f03b5ba704105056f69054cf753040761f84cbe5
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-29 02:44:36 +0300

    Update main.py

--- appointment_service_product/controllers/main.py
+++ appointment_service_product/controllers/main.py
@@ -3,24 +3,12 @@ from odoo.http import request
 
 class AppointmentController(http.Controller):
     
-    @http.route('/appointment', type='http', auth="public", website=True)
-    def appointment_form(self, **post):
+    @http.route('/website/calendar', type='http', auth="public", website=True)
+    def calendar_appointment(self, **post):
         services = request.env['product.product'].search([
             ('type', '=', 'service'),
             ('sale_ok', '=', True)
         ])
-        return request.render("appointment_service_product.appointment_page", {
+        return request.render("website_calendar.index", {
             'services': services,
         })
-    
-    @http.route('/appointment/submit', type='http', auth="public", website=True, csrf=True)
-    def appointment_submit(self, **post):
-        # Create calendar event
-        event = request.env['calendar.event'].create({
-            'name': post.get('name'),
-            'start': post.get('start_datetime'),
-            'stop': post.get('end_datetime'),
-            'product_id': post.get('service_id'),
-            # Add other necessary fields
-        })
-        return request.redirect('/appointment/confirmation')
