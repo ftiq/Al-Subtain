3061037baed946f1ffa6c2155cb27d27010f93d2
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-29 02:50:55 +0300

    Delete appointment_service_product/models directory

--- appointment_service_product/models/calendar_event.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class CalendarEvent(models.Model):
-    _inherit = 'calendar.event'
-
-    product_id = fields.Many2one('product.product', string='الخدمة المختارة')
