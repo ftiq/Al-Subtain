22307196b24d8c0f4d76ff791924338861a90c28
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-28 11:47:50 +0300

    Add files via upload

---
+++ website_appointment_enhanced/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Website Appointment Enhanced",
+    "version": "1.0",
+    "depends": ["appointment", "sale_management", "website"],
+    "data": [
+        "views/appointment_templates.xml"
+    ],
+    "installable": True,
+    "auto_install": False
+}
