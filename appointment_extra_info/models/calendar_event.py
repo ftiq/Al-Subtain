4f574142d080585ec7578f13f70bb05c88848962
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-28 22:22:57 +0300

    Update calendar_event.py

--- appointment_extra_info/models/calendar_event.py
+++ appointment_extra_info/models/calendar_event.py
@@ -3,4 +3,5 @@ from odoo import models, fields
 class CalendarEvent(models.Model):
     _inherit = 'calendar.event'
 
-    x_attachment_id = fields.Many2one('ir.attachment', string="Uploaded File")
+    x_attachment_id = fields.Many2one('ir.attachment', string="المرفق")
+    x_service_id = fields.Many2one('product.product', string="الخدمة")
