c2a86c55a15382d57947445ed4c65d6229287591
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-28 15:18:05 +0300

    Delete website_appointment_enhanced3 directory

--- website_appointment_enhanced3/models/calendar_event.py
+++
@@ -1,7 +0,0 @@
-
-from odoo import models, fields
-
-class CalendarEvent(models.Model):
-    _inherit = 'calendar.event'
-
-    service_id = fields.Many2one('product.product', string='Service Product')
