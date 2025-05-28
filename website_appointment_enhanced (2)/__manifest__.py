444e0bd50b6b43d2d2cafa75ec527ff475053d63
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-28 15:16:00 +0300

    Add files via upload

---
+++ website_appointment_enhanced (2)/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Website Appointment Enhanced",
+    "version": "1.0",
+    "category": "Website",
+    "summary": "Add service selection and attachment to appointment form",
+    "depends": ["website_appointment", "sale"],
+    "data": [
+        "views/appointment_templates.xml"
+    ],
+    "application": True,
+    "installable": True,
+    "license": "LGPL-3"
+}
