85a4fb18862e1bbf7123a57558a91d59c7d889be
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-29 01:45:03 +0300

    Update __manifest__.py

--- appointment_service_product/__manifest__.py
+++ appointment_service_product/__manifest__.py
@@ -1,15 +1,36 @@
+# -*- coding: utf-8 -*-
 {
-    'name': 'Custom Appointment Products',
-    'version': '1.0',
-    'summary': 'Add product selection to website appointments',
-    'description': 'Allows customers to select products/services when booking appointments',
-    'author': 'Your Name',
-    'depends': ['website_appointment', 'product'],
+    'name': "Appointment Service Product Selection",
+    'summary': """
+        Add product/service selection to website appointments""",
+    'description': """
+        This module extends website appointments to allow customers
+        to select a specific product/service when booking appointments.
+    """,
+    'author': "Your Name",
+    'website': "http://www.yourcompany.com",
+    'category': 'Services/Appointment',
+    'version': '18.0.1.0.0',
+    'depends': [
+        'website_appointment',  # Main appointment module
+        'website',              # Website functionality
+        'product',             # Product management
+    ],
     'data': [
         'security/ir.model.access.csv',
         'views/appointment_views.xml',
         'views/appointment_templates.xml',
     ],
+    'demo': [
+        'demo/product_demo.xml',
+    ],
     'installable': True,
     'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+    'assets': {
+        'web.assets_frontend': [
+            'appointment_service_product/static/src/js/website_appointment.js',
+        ],
+    },
 }
