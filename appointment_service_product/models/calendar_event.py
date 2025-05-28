343e74f024c91df27178d0193495ac28aafa4988
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-29 02:44:06 +0300

    Update calendar_event.py

--- appointment_service_product/models/calendar_event.py
+++ appointment_service_product/models/calendar_event.py
@@ -2,5 +2,5 @@ from odoo import models, fields
 
 class CalendarEvent(models.Model):
     _inherit = 'calendar.event'
-
-    product_id = fields.Many2one('product.product', string='Selected Service')
+    
+    product_id = fields.Many2one('product.product', string='الخدمة المختارة')
