2590347be82648abf819f46943e6c32d2e7d7571
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-29 01:30:57 +0300

    Update __manifest__.py

--- appointment_service_product/__manifest__.py
+++ appointment_service_product/__manifest__.py
@@ -3,7 +3,7 @@
     "summary": "Add service product selection to appointment booking",
     "version": "1.0",
     "category": "Website",
-    "depends": [ "product"],
+    "depends": ["website", "product"],
     "data": [
         "views/appointment_info_inherit.xml",
     ],
