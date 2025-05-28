49e516f4870f0f0629c456021da4cbbbaf0333c9
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-28 21:49:26 +0300

    Add files via upload

---
+++ appointment_extra_info/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Appointment Extra Info",
+    "version": "1.0",
+    "depends": ["website", "appointment", "sale_management"],
+    "category": "Website",
+    "summary": "Add a separate page for service and attachment after appointment confirmation",
+    "data": [
+        "views/appointment_extra_template.xml"
+    ],
+    "installable": True,
+    "application": False
+}
