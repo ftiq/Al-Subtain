da1338707935216806beb10906c94145fb853e3d
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-29 01:47:37 +0300

    Update and rename appointment.py to calendar_event.py

---
+++ appointment_service_product/models/calendar_event.py
@@ -0,0 +1,10 @@
+from odoo import models, fields
+
+class CalendarEvent(models.Model):
+    _inherit = 'calendar.event'
+
+    product_id = fields.Many2one(
+        'product.product',
+        string='Service/Product',
+        domain=[('type', '=', 'service')]
+    )
