582f8c72c1e537e010eff2887a410b4bcf0940a6
MHD Manar Zamrik <manar.z@ft.iq>
2025-06-02 16:16:00 +0300

    Update __manifest__.py

--- sample_negative_qty_display/__manifest__.py
+++ sample_negative_qty_display/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Sample Negative Quantity with Absolute Display",
-    "version": "1.0",
+    "version": "1.1",
     "category": "Sales",
     "summary": "Show only display quantity for a specific product as positive in Sale Order lines",
     "depends": ["sale_management"],
