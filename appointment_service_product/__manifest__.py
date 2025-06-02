6a0d5f0ec24513d775bc4e3537f5615f9d1e0d43
MHD Manar Zamrik <manar.z@ft.iq>
2025-06-02 13:10:48 +0300

    Delete appointment_service_product directory

--- appointment_service_product/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    'name': 'Add Service Field',
-    'version': '1.0',
-    'depends': ['website', 'product'],
-    'data': [
-        'views/website_templates.xml',
-    ],
-    'installable': True,
-}
