eff646c606923b34093c98ed5fff1a158ccee113
MHD Manar Zamrik <manar.z@ft.iq>
2025-06-02 16:29:31 +0300

    Update __manifest__.py

--- sample_negative_qty_display/__manifest__.py
+++ sample_negative_qty_display/__manifest__.py
@@ -4,9 +4,7 @@
     "category": "Sales",
     "summary": "Show only display quantity for a specific product as positive in Sale Order lines",
     "depends": ["sale_management"],
-    "data": [
-        "views/sale_order_line_view.xml"
-    ],
+ 
     "installable": True,
     "application": False,
     "license": "LGPL-3"
