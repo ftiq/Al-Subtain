8a24caeaa856a83957475902ba0edfcf22f4e13c
MHD Manar Zamrik <manar.z@ft.iq>
2025-06-03 16:06:34 +0300

    Update sale_order_line.py

--- sample_negative_qty_display/models/sale_order_line.py
+++ sample_negative_qty_display/models/sale_order_line.py
@@ -1,7 +1,5 @@
 from odoo import models, fields, api
 
-SAMPLE_PRODUCT_ID = 735
-
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
@@ -12,25 +10,34 @@ class SaleOrderLine(models.Model):
         store=False,
     )
 
+    @api.model
+    def _get_sample_product_id(self):
+        # جلب id من xml id
+        product = self.env.ref('__export__.product_template_735_e00f2af9', raise_if_not_found=False)
+        return product.id if product else False
+
     @api.depends('product_id', 'product_uom_qty')
     def _compute_display_qty(self):
+        sample_id = self._get_sample_product_id()
         for line in self:
-            if line.product_id and line.product_id.id == SAMPLE_PRODUCT_ID:
+            if line.product_id and line.product_id.id == sample_id:
                 line.display_qty = abs(line.product_uom_qty)
             else:
                 line.display_qty = line.product_uom_qty
 
     def _inverse_display_qty(self):
+        sample_id = self._get_sample_product_id()
         for line in self:
-            if line.product_id and line.product_id.id == SAMPLE_PRODUCT_ID:
+            if line.product_id and line.product_id.id == sample_id:
                 line.product_uom_qty = -abs(line.display_qty)
             else:
                 line.product_uom_qty = line.display_qty
 
     @api.onchange('display_qty', 'product_id')
     def _onchange_display_qty(self):
+        sample_id = self._get_sample_product_id()
         for line in self:
-            if line.product_id and line.product_id.id == SAMPLE_PRODUCT_ID:
+            if line.product_id and line.product_id.id == sample_id:
                 line.product_uom_qty = -abs(line.display_qty)
             else:
                 line.product_uom_qty = line.display_qty
