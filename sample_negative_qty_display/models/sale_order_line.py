bed74e9313ec3166dd00e6fcc8d11409ebe21560
MHD Manar Zamrik <manar.z@ft.iq>
2025-06-03 16:28:29 +0300

    Update sale_order_line.py

--- sample_negative_qty_display/models/sale_order_line.py
+++ sample_negative_qty_display/models/sale_order_line.py
@@ -10,16 +10,21 @@ class SaleOrderLine(models.Model):
         store=False,
     )
 
-    @api.depends('product_uom_qty')
+    @api.depends('product_uom_qty', 'product_id.product_tmpl_id')
     def _compute_display_qty(self):
         for line in self:
-            line.display_qty = abs(line.product_uom_qty)
+            if line.product_id.product_tmpl_id.id == 735:  # تحقق من قالب المنتج 735
+                line.display_qty = abs(line.product_uom_qty)
+            else:
+                line.display_qty = line.product_uom_qty
 
     def _inverse_display_qty(self):
         for line in self:
-            line.product_uom_qty = -abs(line.display_qty)
+            if line.product_id.product_tmpl_id.id == 735:  # تحقق من قالب المنتج 735
+                line.product_uom_qty = -abs(line.display_qty)
 
     @api.onchange('display_qty')
     def _onchange_display_qty(self):
         for line in self:
-            line.product_uom_qty = -abs(line.display_qty)
+            if line.product_id.product_tmpl_id.id == 735:  # تحقق من قالب المنتج 735
+                line.product_uom_qty = -abs(line.display_qty)
