6835dde55ed6927f6c996598aed5aa9099dc48cc
MHD Manar Zamrik <manar.z@ft.iq>
2025-06-03 15:55:34 +0300

    Update sale_order_line.py

--- sample_negative_qty_display/models/sale_order_line.py
+++ sample_negative_qty_display/models/sale_order_line.py
@@ -1,5 +1,7 @@
 from odoo import models, fields, api
 
+SAMPLE_PRODUCT_ID = 735  # رقم المادة التي تريد عكس الإشارة لها فقط
+
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
@@ -10,16 +12,25 @@ class SaleOrderLine(models.Model):
         store=False,
     )
 
-    @api.depends('product_uom_qty')
+    @api.depends('product_id', 'product_uom_qty')
     def _compute_display_qty(self):
         for line in self:
-            line.display_qty = abs(line.product_uom_qty)
+            if line.product_id and line.product_id.id == SAMPLE_PRODUCT_ID:
+                line.display_qty = abs(line.product_uom_qty)
+            else:
+                line.display_qty = line.product_uom_qty
 
     def _inverse_display_qty(self):
         for line in self:
-            line.product_uom_qty = -abs(line.display_qty)
+            if line.product_id and line.product_id.id == SAMPLE_PRODUCT_ID:
+                line.product_uom_qty = -abs(line.display_qty)
+            else:
+                line.product_uom_qty = line.display_qty
 
-    @api.onchange('display_qty')
+    @api.onchange('display_qty', 'product_id')
     def _onchange_display_qty(self):
         for line in self:
-            line.product_uom_qty = -abs(line.display_qty)
+            if line.product_id and line.product_id.id == SAMPLE_PRODUCT_ID:
+                line.product_uom_qty = -abs(line.display_qty)
+            else:
+                line.product_uom_qty = line.display_qty
