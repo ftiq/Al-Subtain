9cace84de11d77bea67095b06cf3c23d4b270d95
MHD Manar Zamrik <manar.z@ft.iq>
2025-06-17 23:08:48 +0300

    Add files via upload

---
+++ appointment_products/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from . import appointment_product
+from . import appointment_sale_answer_input
+from . import fsm_task_answer
+from . import product_service_flag
+from . import fsm_task_service_line
+from . import project_task_signature_wizard
