357dbadf8ae0fbc0d4719183053620599ce118fc
saleh alhjany <salehalhjany2025@gmail.com>
2025-09-10 22:27:16 +0300

    Update lab_sample_clean.py

--- appointment_products/models/lab_sample_clean.py
+++ appointment_products/models/lab_sample_clean.py
@@ -1771,12 +1771,11 @@ class LabSample(models.Model):
         """
         
         return html
-    
-    def unlink(self):
-        """منع حذف العينات في حالة الاختبار أو المكتملة أو المصادق عليها"""
-        for sample in self:
-            if sample.state in ('testing', 'completed', 'approved'):
-                from odoo.exceptions import UserError
-                raise UserError(_('لا يمكن حذف العينة في حالة الاختبار أو بعد اكتمالها أو بعد المصادقة عليها.'))
-        return super().unlink()
- 
+      
+   # def unlink(self):
+   #     """منع حذف العينات في حالة الاختبار أو المكتملة أو المصادق عليها"""
+   #     for sample in self:
+   #         if sample.state in ('testing', 'completed', 'approved'):
+   #             from odoo.exceptions import UserError
+   #             raise UserError(_('لا يمكن حذف العينة في حالة الاختبار أو بعد اكتمالها أو بعد المصادقة عليها.'))
+   #     return super().unlink()
