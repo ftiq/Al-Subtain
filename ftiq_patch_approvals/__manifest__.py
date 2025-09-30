4ea09143b4b53fd76760643ea2ca9661771b69dc
MHD Manar Zamrik <manar.z@ft.iq>
2025-09-30 17:14:11 +0300

    Update __manifest__.py

--- ftiq_patch_approvals/__manifest__.py
+++ ftiq_patch_approvals/__manifest__.py
@@ -1,9 +1,9 @@
 {
     "name": "FTIQ Patch Approvals",
     "version": "18.0.1.0.0",
-    "summary": "Patch to disable broken approval.request field",
-    "depends": ["approvals"],
+    "summary": "Emergency patch to disable broken approval.request field",
+    "depends": [],  # بدون أي موديولات
     "data": [],
     "installable": True,
-    "auto_install": False,
+    "auto_install": True,  # ينزل تلقائي مع أول تشغيل
 }
